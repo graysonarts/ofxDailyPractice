87a2bcee9bc51e517e6ae679ab7908ae39184e08
Grayson Hay <me@russellhay.com>
2020-06-04 04:55:08 -0700

    RYBk2.3

--- RYBk/src/sketches/sketches.h
+++ RYBk/src/sketches/sketches.h
@@ -4,6 +4,7 @@
 #include "k1_7.h"
 #include "k2_1.h"
 #include "k2_2.h"
+#include "k2_3.h"
 /// INCLUDE
 
 #include "shame.h"
@@ -19,6 +20,7 @@ std::vector<std::unique_ptr<IDailySketch> > getSketches(ofApp* parent) {
 	std::vector<std::unique_ptr<IDailySketch> > retval;
 
 	/// SKETCH LIST
+	SKETCH(k2_3);
 	SKETCH(k2_2);
 	SKETCH(k2_1);
 	SKETCH(k1_7);
