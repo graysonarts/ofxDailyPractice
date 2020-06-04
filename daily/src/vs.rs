569c1614ffb93a0acf0b325c39b532aec9858f7e
Grayson Hay <me@russellhay.com>
2020-06-04 05:07:15 -0700

    Fixes filters file editing

--- daily/src/vs.rs
+++ daily/src/vs.rs
@@ -1,8 +1,10 @@
 use std::path::{ PathBuf, Path};
 use std::io::prelude::*;
 
-const VCXPROJ_TAG_LINE : &'static str = r#"<ClInclude Include="src\sketches\shame.h"#;
-
+const VCXPROJ_TAG_LINE : &'static str = r#"<ClInclude Include="src\sketches\sketches.h" />"#;
+const VCXPROJ_FILTER_LINE : &'static str = r#"<ClInclude Include="src\sketches\shame.h">
+      <Filter>src\sketches</Filter>
+    </ClInclude>"#;
 
 pub fn has_project(path: &Path) -> bool {
 	vcxproj(path).exists()
@@ -37,11 +39,11 @@ pub fn add_to_vcxproj(path: &Path, name: &str) -> std::io::Result<()> {
 pub fn add_to_filters(path: &Path, name: &str) -> std::io::Result<()> {
 	let filename = filters(path);
 	let data = std::fs::read_to_string(&filename)?;
-	let parts : Vec<_> = data.split(VCXPROJ_TAG_LINE).collect();
+	let parts : Vec<_> = data.split(VCXPROJ_FILTER_LINE).collect();
 	let mut output_file = std::fs::File::create(filename)?;
 	write!(output_file, "{}", parts[0])?;
-	write!(output_file, r#"<ClInclude Include="src\sketches\{}.h" />"#, name)?;
+	write!(output_file, r#"<ClInclude Include="src\sketches\{}.h">"#, name)?;
 	write!(output_file, "\n      <Filter>src\\sketches</Filter>\n    </ClInclude>\n")?;
-	write!(output_file, "    {}", VCXPROJ_TAG_LINE)?;
+	write!(output_file, "    {}", VCXPROJ_FILTER_LINE)?;
 	write!(output_file, "{}", parts[1])
 }
